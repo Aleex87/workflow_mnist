ef2d22523768b80c21ad1d93ec64d354b71bbe82
Alicia <alicia.gezelius@gmail.com>
2026-02-27 16:11:21 +0100

    added classes to schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -5,10 +5,15 @@ Responsibilities:
 - Define input schema for prediction
 - Define response schema
 """
-from fastAPI import FastAPI
+from pydantic import BaseModel, Field, conlist, confloat
 
+amount_of_pixels = confloat(ge=0, le=16)
+#conflotat gives a float between 0 and 16
 
-class request(Basemodel):
-    pred: int = Filed
+class Request(BaseModel):
+    features: conlist(amount_of_pixels, min_lenght=64, max_length=64)
+#give amount pf pixels and define the amount of pixels
 
-
+class Prediction(BaseModel):
+    predicrtion: int = Field(ge=0, le=9)
+    confidence: float = Field(ge=0.0, le=1.0)
