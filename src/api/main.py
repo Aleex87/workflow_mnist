278c3b3df0ebcb266c9888072faafae2a9f5ff01
Alicia <alicia.gezelius@gmail.com>
2026-02-27 10:33:50 +0100

    started on model loader

--- src/api/main.py
+++ src/api/main.py
@@ -6,6 +6,13 @@ Responsibilities:
 - Load exported TorchScript model
 - Define POST /predict endpoint
 """
+import fastAPI 
+import torch
+from os import path
+ 
+
+
+
 
 def main():
     print("Hello from workflow-minist!")
--- src/api/model_loader.py
+++ src/api/model_loader.py
@@ -5,3 +5,13 @@ Responsibilities:
 - Load TorchScript model from artifacts/
 - Prepare model for inference
 """
+from pathlib import Path
+import torch
+
+arifact_path = Path(__file__).resolve().parents[2] / "artifacts"
+model_path = arifact_path / "digits_mlp.ts"
+
+def loading_model():
+    model = torch.jit.load(str(model_path), map_location="cpu")
+    model.eval()
+    return model
